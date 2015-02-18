9bd27e97794cae4f6dea5f5e4aebcaefb453d010
David Raluy <draluy@yahoo.fr>
2015-02-18 21:26:55 +0100

    The main page now transitions properly to the topics page.

--- js/HFRMainController.js
+++ js/HFRMainController.js
@@ -7,41 +7,30 @@ function ()
 
 	var processTemplate = function (data)
 	{
-		var resultText = Mustache.render(this, data);
-		document.querySelector("#content").innerHTML = resultText;
+		var template = document.querySelector("#drawer").innerHTML;
+		var rendered = Mustache.render(template, data);
+		document.querySelector("#drawer").innerHTML = rendered;	
 
 		//Add event listeners
 		for (var i = 0; i < data.categories.length; i++) {
 			var cat = data.categories[i];
-			var domNode  = document.querySelector('a#subcategory_'+cat.id);
-			/*domNode.addEventListener("click", 
-				(function (catId)
-				{
-					return function (){ HFRFOS.SubCategoriesController.displaySubCats(catId, "#content");};
-				})(cat.id)
-				, false);*/
 
 			var domNode  = document.querySelector('a#category_'+cat.id);
 			domNode.addEventListener("click", 
 				(function (catId)
 				{
-					return function (){ HFRFOS.TopicsController.displayTopics( "#content", catId);};
+					return function (){ HFRFOS.TopicsController.displayTopics(catId);};
 				})(cat.id)
 				, false);
 		};
-		
-	};
 
-	var  getViewTemplate= function (data)
-	{
-		HFRFOS.DataRetriever.getCategories(processTemplate.bind(data));
 	};
 
 	HFRFOS.MainController.displayHomeView = function ()
 	{
-		var viewRequest = new HFRFOS.Common.Ajax(getViewTemplate,"get","views/HomeView.html");
-		viewRequest.send(null);
-	};
+		
+		HFRFOS.DataRetriever.getCategories(processTemplate);
+	}
 
 	window.onload = function ()
 	{
--- js/HFRTopicsController.js
+++ js/HFRTopicsController.js
@@ -3,33 +3,31 @@ this.HFRFOS.TopicsController = {};
 (function ()
 {
 
-	HFRFOS.TopicsController.displayTopics = function (idToDisplayTo, catId, subcatId)
+	HFRFOS.TopicsController.displayTopics = function (catId, subcatId)
 	{
-		var viewRequest = new HFRFOS.Common.Ajax(function (templateData){
-			HFRFOS.DataRetriever.getTopics (function (data){
+		HFRFOS.DataRetriever.getTopics (function (data){
+			var template = document.querySelector("#topics").innerHTML;
+			var resultText = Mustache.render(template, data);
+			document.querySelector("#topics").innerHTML = resultText;
 
-				
+			for (var i = 0; i < data.topics.length; i++) {
+				var topic = data.topics[i];
+				var domNode  = document.querySelector('a#topic_'+topic.id);
 
-				console.log(data);
-				console.log(templateData);
-				var resultText = Mustache.render(templateData, data);
-				document.querySelector(idToDisplayTo).innerHTML = resultText;
+				domNode.addEventListener("click", 
+				(function (topicId)
+				{
+					return function (){ HFRFOS.PostsController.displayPosts("#content", catId, topicId);};
+				})(topic.id)
+				, false);
+			}
 
-				for (var i = 0; i < data.topics.length; i++) {
-					var topic = data.topics[i];
-					var domNode  = document.querySelector('a#topic_'+topic.id);
+			//shift the section in place
+			/*document.querySelector("#drawer").classList.remove("current");
+			document.querySelector("#drawer").classList.add("left");*/
+			document.querySelector("#topics").className = "current";
 
-					domNode.addEventListener("click", 
-					(function (topicId)
-					{
-						return function (){ HFRFOS.PostsController.displayPosts("#content", catId, topicId);};
-					})(topic.id)
-					, false);
-				}
-
-			},catId, subcatId);
-
-		},"get","views/TopicsView.html");
-		viewRequest.send(null);
-	};
+		},catId, subcatId);
+	}
+	
 })();
